d32510b0ff0ea88a67e9addf2671d47590582b25
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-17 09:07:54 -0600

    Refactor: routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,10 @@
 Rails.application.routes.draw do
-  get '/encryptions/new', to: 'encryptions#new'
-  post '/encryptions', to: 'encryptions#create'
-  get '/encryptions/:id', to: 'encryptions#show'
-  delete '/encryptions/:id', to: 'encryptions#destroy'
+  resources :encryptions, only: [:create, :new, :show, :destroy]
   get '/encryptions/:id/confirmation', to: 'encryptions#confirmation'
-  # resources :encryptions, only: [:new]
 
-  get '/decryptions/new', to: 'decryptions#new'
-  post '/decryptions', to: 'decryptions#create'
-  get '/decryptions/:id', to: 'decryptions#show'
-  delete '/decryptions/:id', to: 'decryptions#destroy'
+  resources :decryptions, only: [:create, :new, :show, :destroy]
   get '/decryptions/:id/confirmation', to: 'decryptions#confirmation'
-  # resources :decryptions, only: [:new]
 
-  get '/cracks/new', to: 'cracks#new'
-  post '/cracks', to: 'cracks#create'
-  get '/cracks/:id', to: 'cracks#show'
-  delete '/cracks/:id', to: 'cracks#destroy'
+  resources :cracks, only: [:create, :new, :show, :destroy]
   get '/cracks/:id/confirmation', to: 'cracks#confirmation'
-  # resources :cracks, only: [:new]
 end
