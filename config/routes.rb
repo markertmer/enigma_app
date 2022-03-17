904c7d587e8942f2bb9c9894b3ff33c017f7bd6d
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-17 08:54:12 -0600

    Add: routing adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,22 @@
 Rails.application.routes.draw do
   get '/encryptions/new', to: 'encryptions#new'
-  post '/encryptions/new', to: 'encryptions#create'
+  post '/encryptions', to: 'encryptions#create'
   get '/encryptions/:id', to: 'encryptions#show'
   delete '/encryptions/:id', to: 'encryptions#destroy'
   get '/encryptions/:id/confirmation', to: 'encryptions#confirmation'
-  # resources :encryptions, only:[:new, :create, :show, :destroy, :confirmation]
+  # resources :encryptions, only: [:new]
 
   get '/decryptions/new', to: 'decryptions#new'
-  post '/decryptions/new', to: 'decryptions#create'
+  post '/decryptions', to: 'decryptions#create'
   get '/decryptions/:id', to: 'decryptions#show'
   delete '/decryptions/:id', to: 'decryptions#destroy'
   get '/decryptions/:id/confirmation', to: 'decryptions#confirmation'
-  # resources :encryptions, only:[:new, :create, :show, :destroy, :confirmation]
+  # resources :decryptions, only: [:new]
 
   get '/cracks/new', to: 'cracks#new'
-  post '/cracks/new', to: 'cracks#create'
+  post '/cracks', to: 'cracks#create'
   get '/cracks/:id', to: 'cracks#show'
   delete '/cracks/:id', to: 'cracks#destroy'
   get '/cracks/:id/confirmation', to: 'cracks#confirmation'
+  # resources :cracks, only: [:new]
 end
