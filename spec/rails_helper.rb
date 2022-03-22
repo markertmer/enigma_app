fcb3d7b4a70e5648f5d54da0e78c7727e15f301c
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-22 17:32:50 -0600

    Update: Rails helper shoulda-matchers, string interpolation in view

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -47,11 +47,11 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
 
 
-# Shoulda::Matchers.configure do |config|
-#   config.integrate do |with|
-#     with.test_framework :rspec
-#     with.library :rails
-#   end
-# end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
 
 end
